7b1a533e46164e863e288480787b6d96ba87b2bf
humayun-dev <humayun.devv@gmail.com>
2025-02-07 14:19:56 +0500

    updated

--- app.py
+++ app.py
@@ -261,5 +261,5 @@ def process_video_api():
 def download_video(filename):
     return send_from_directory(PROCESSED_FOLDER, filename)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
