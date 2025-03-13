5ce362030fcec7018184b3693cdcb21759e059d6
Muhammad Kamran Khan <mkkofficial74@gmail.com>
2025-03-14 02:33:32 +0500

    update models path

--- app.py
+++ app.py
@@ -263,8 +263,8 @@ async def process_video(
             
             # Step 2: Process the frames using YOLO and SAM models.
             # (Update the model paths as needed.)
-            yolo_model_path = "lastnew2.pt"
-            sam_model_path = "sam2.1_l.pt"
+            yolo_model_path = "spin360/lastnew.pt"
+            sam_model_path = "spin360/sam2.1_l.pt"
             processed = process_frames_with_depth_and_individual_tints(frames_dir, processed_dir, yolo_model_path, sam_model_path)
             if not processed:
                 return JSONResponse(
