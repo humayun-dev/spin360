dd81d8cf1ef8c5e7a9982a6f5b45e222f39a165f
Muhammad Kamran Khan <mkkofficial74@gmail.com>
2025-03-15 02:17:13 +0500

    add missing libraries

--- app.py
+++ app.py
@@ -374,5 +374,5 @@ async def delete_video(filename: str):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
